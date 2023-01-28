1c690475a9dee1f3059ddf8ed6f054f69bd83960
Jerry Nettleton <jnettleton@gmail.com>
2023-01-27 23:43:56 -0600

    save code

--- tft-spi-demo/src/main.rs
+++ tft-spi-demo/src/main.rs
@@ -5,8 +5,6 @@ use rpi_display::rpi_tft_display::ST7735Color;
 use rpi_display::rpi_tft_display::RpiTftDisplay;
 
 fn main() -> io::Result<()> {
-    println!("Hello, world!");
-
     let mut tft_display = RpiTftDisplay::new();
     tft_display.initialize()?;
     tft_display.fill_screen(ST7735Color::RED as u16)?;
